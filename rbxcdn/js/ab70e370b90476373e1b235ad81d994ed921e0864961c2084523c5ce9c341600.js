15de925875877257452318ef6fd7cd2e938d243e
beak <27046219+floatzeI@users.noreply.github.com>
2021-12-09 14:24:17 -0500

    add rbxcdn

---
+++ rbxcdn/js/ab70e370b90476373e1b235ad81d994ed921e0864961c2084523c5ce9c341600.js
@@ -0,0 +1,4 @@
+var Roblox = Roblox || {};
+Roblox.LangDynamic = Roblox.LangDynamic || {};
+Roblox.LangDynamic["Feature.SearchDropdownComponent"] = {"Label.GroupName":"Group Name","Label.Username":"Username"};
+window.Roblox && window.Roblox.BundleDetector && window.Roblox.BundleDetector.bundleDetected("DynamicLocalizationResourceScript_Feature.SearchDropdownComponent");
---
+++ rbxcdn/js/bffc0aac08ffa2b127684bc99cffeb9263cfcabe41f02cf9ca716792e1fa4d0f.js
@@ -0,0 +1,4 @@
+var Roblox = Roblox || {};
+Roblox.LangDynamicDefault = Roblox.LangDynamicDefault || {};
+Roblox.LangDynamicDefault["Feature.SearchDropdownComponent"] = {"Label.GroupName":"Group Name","Label.Username":"Username"};
+window.Roblox && window.Roblox.BundleDetector && window.Roblox.BundleDetector.bundleDetected("DynamicLocalizationResourceScript_Feature.SearchDropdownComponent");
